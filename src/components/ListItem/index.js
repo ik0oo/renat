a696d751701994da7a68d53fa7d6b9257a0826fd
d.kichigin <d.kichigin@space307.com>
2018-11-02 16:30:20 +0300

    added image to list items

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+import defaultIcon from '../../../assets/58873055-white-place-icon-on-blue-button-isolated-on-white.jpg';
 
-export default ({ content, onPressItem }) => (
+export default ({ content, onPressItem, icon }) => (
     <TouchableOpacity onPress={onPressItem}>
         <View style={styles.item}>
+            <Image source={icon || defaultIcon} style={styles.image} />
             <Text style={{fontSize: 20}}>{content}</Text>
         </View>
     </TouchableOpacity>
@@ -15,7 +17,13 @@ const styles = StyleSheet.create({
         marginBottom: 3, 
         backgroundColor: '#eee',
         justifyContent: 'flex-start',
+        flexDirection: 'row',
         width: '100%',
-        alignItems: 'flex-start',
+        alignItems: 'center',
     },
+    image: {
+        width: 20,
+        height: 20,
+        marginRight: 10,
+    }
 });
